fe53ced21f902007004800125b3341ca3060abbb
Leland McInnes <leland.mcinnes@gmail.com>
2019-10-04 14:00:25 -0400

    Add leaf size option, make use of queue_size

--- ann_benchmarks/algorithms/pynndescent.py
+++ ann_benchmarks/algorithms/pynndescent.py
@@ -6,13 +6,14 @@ import numpy as np
 
 class PyNNDescent(BaseANN):
     def __init__(self, metric, n_neighbors=10, pruning_degree_multiplier=2.0,
-                 diversify_epsilon=0.5, n_jobs=1):
+                 diversify_epsilon=0.5, leaf_size=30, n_jobs=1):
         self._n_neighbors = int(n_neighbors)
         self._queue_size = None
         self._n_search_trees = 0
         self._n_jobs = int(n_jobs)
         self._pruning_degree_multiplier = float(pruning_degree_multiplier)
         self._diversify_epsilon = float(diversify_epsilon)
+        self._leaf_size = int(leaf_size)
         self._pynnd_metric = {'angular': 'cosine',
                               'euclidean': 'euclidean',
                               'hamming': 'hamming',
@@ -23,7 +24,7 @@ class PyNNDescent(BaseANN):
                                             n_neighbors=self._n_neighbors,
                                             metric=self._pynnd_metric,
                                             low_memory=True,
-                                            leaf_size=80,
+                                            leaf_size=self._leaf_size,
                                             pruning_degree_multiplier=self._pruning_degree_multiplier,
                                             diversify_epsilon=self._diversify_epsilon,
                                             n_jobs=self._n_jobs)
