c9106a59d3ef0e858b8bc5adc068448bf0d84e32
Leland McInnes <leland.mcinnes@gmail.com>
2019-11-04 12:00:04 -0500

    Updates for upcoming pynndescent release

--- ann_benchmarks/algorithms/pynndescent.py
+++ ann_benchmarks/algorithms/pynndescent.py
@@ -6,11 +6,10 @@ import numpy as np
 
 class PyNNDescent(BaseANN):
     def __init__(self, metric, n_neighbors=10, pruning_degree_multiplier=2.0,
-                 diversify_epsilon=0.5, leaf_size=30, n_jobs=1):
+                 diversify_epsilon=0.5, leaf_size=30):
         self._n_neighbors = int(n_neighbors)
         self._queue_size = None
         self._n_search_trees = 0
-        self._n_jobs = int(n_jobs)
         self._pruning_degree_multiplier = float(pruning_degree_multiplier)
         self._diversify_epsilon = float(diversify_epsilon)
         self._leaf_size = int(leaf_size)
@@ -23,17 +22,16 @@ class PyNNDescent(BaseANN):
         self._index = pynndescent.NNDescent(X.astype(np.float32, order='C'),
                                             n_neighbors=self._n_neighbors,
                                             metric=self._pynnd_metric,
-                                            low_memory=True,
+                                            low_memory=False,
                                             leaf_size=self._leaf_size,
                                             pruning_degree_multiplier=self._pruning_degree_multiplier,
-                                            diversify_epsilon=self._diversify_epsilon,
-                                            n_jobs=self._n_jobs)
+                                            diversify_epsilon=self._diversify_epsilon)
         self._index._init_search_graph()
 
-    def set_query_arguments(self, queue_size, epsilon=0.1, n_search_trees=1):
-        self._queue_size = float(queue_size)
-        self._n_search_trees = int(n_search_trees)
+    def set_query_arguments(self, epsilon=0.1, n_search_trees=1, queue_size=1.0):
         self._epsilon = float(epsilon)
+        self._n_search_trees = int(n_search_trees)
+        self._queue_size = float(queue_size)
 
     def query(self, v, n):
         ind, dist = self._index.query(
@@ -44,8 +42,9 @@ class PyNNDescent(BaseANN):
         return ind[0]
 
     def __str__(self):
-        str_template = ('PyNNDescent(n_neighbors=%d, pruning_degree_multiplier=%.1f, epsilon=%.3f, '
-                        'diversify_epsilon=%.2f)')
-        return str_template % (self._n_neighbors, self._pruning_degree_multiplier, self._epsilon,
-                               self._diversify_epsilon)
+        str_template = ('PyNNDescent(n_neighbors=%d, pruning_degree_multiplier=%.1f, '
+                        'diversify_epsilon=%.2f, epsilon=%.3f)')
+        return str_template % (self._n_neighbors, self._pruning_degree_multiplier,
+                               self._diversify_epsilon, self._epsilon,
+                               )
 
